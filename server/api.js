70875f0d9ad95049aa40e60f86e4f1b189c0254a
Chrismmint <33577525+Chrismmint@users.noreply.github.com>
2018-01-23 15:42:51 +0000

    Bonus not done

--- server/api.js
+++ server/api.js
@@ -1,6 +1,92 @@
 const express = require('express');
 const apiRouter = express.Router();
+const {
+  createMeeting,
+  getAllFromDatabase,
+  getFromDatabaseById,
+  addToDatabase,
+  updateInstanceInDatabase,
+  deleteFromDatabasebyId,
+  deleteAllFromDatabase,
+} = require('./db');
+const checkMillionDollarIdea = require('./checkMillionDollarIdea');
 
+apiRouter.param('minionId', (req, res, next, id) => {
+  if (id.isNaN) {
+    res.status(404).send("NaN")
+  } else {
+    req.minionId = id
+    minion = getFromDatabaseById('minions', id);
+    if (minion) {
+      req.minion = minion;
+      next()
+    } else {
+      res.status(404).send();
+    }
+  }
+})
 
+apiRouter.get('/minions', (req, res, next) => {
+  const minions = getAllFromDatabase('minions');
+  res.status(200).send(minions);
+})
+
+apiRouter.get('/minions/:minionId', (req, res, next) => {
+  res.send(req.minion);
+})
+
+apiRouter.put('/minions/:minionId', (req, res, next) => {
+  const updatedMinion = updateInstanceInDatabase('minions', req.body);
+  res.send(updatedMinion);
+})
+
+apiRouter.post('/minions', (req, res, next) => {
+  addToDatabase('minions', req.body)
+  res.status(201).send(req.body);
+})
+
+apiRouter.delete('/minions/:minionId', (req, res, next) => {
+  deleteFromDatabasebyId('minions', req.minionId);
+  res.status(204).send("Minion Deleted");
+})
+
+apiRouter.param('ideaId', (req, res, next, id) => {
+  if (id.isNaN) {
+    res.status(404).send("NaN")
+  } else {
+    req.ideaId = id
+    const idea = getFromDatabaseById('ideas', id);
+    if (idea) {
+      req.idea = idea;
+      next()
+    } else {
+      res.status(404).send();
+    }
+  }
+})
+
+apiRouter.get('/ideas', (req, res, next) => {
+  const minions = getAllFromDatabase('ideas');
+  res.status(200).send(minions);
+})
+
+apiRouter.get('/ideas/:ideaId', (req, res, next) => {
+  res.send(req.idea);
+})
+
+apiRouter.put('/ideas/:ideaId', (req, res, next) => {
+  const updatedIdea = updateInstanceInDatabase('ideas', req.body);
+  res.send(updatedIdea);
+})
+
+apiRouter.post('/ideas', checkMillionDollarIdea, (req, res, next) => {
+    addToDatabase('ideas', req.body)
+  res.status(201).send(req.body);
+})
+
+apiRouter.delete('/ideas/:ideaId', (req, res, next) => {
+  deleteFromDatabasebyId('ideas', req.ideaId);
+  res.status(204).send("Idea Deleted");
+})
 
 module.exports = apiRouter;
--- server/checkMillionDollarIdea.js
+++ server/checkMillionDollarIdea.js
@@ -1,4 +1,17 @@
-const checkMillionDollarIdea = () => {};
+
+
+const checkMillionDollarIdea = (req, res, next) => {
+  if (typeof req.body.numWeeks === 'number' && typeof req.body.weeklyRevenue === 'number') {
+    const yield = req.body.numWeeks * req.body.weeklyRevenue;
+    if (yield < 1000000) {
+      res.status(400).send();
+    } else {
+      next();
+    }
+  } else {
+    res.status(400).send();
+  }
+};
 
 // Leave this exports assignment so that the function can be used elsewhere
 module.exports = checkMillionDollarIdea;
